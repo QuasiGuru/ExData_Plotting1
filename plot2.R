fe1dbb88729a0a854e7664d4e741f17657ae907e
James Lillico <jay.lillico@gmail.com>
2014-06-03 14:20:18 -0400

    Plot 2 Completed

--- plot2.R
+++ plot2.R
@@ -1,6 +1,6 @@
 #The following program reads data from the UC Irvine Machine Learning 
 #Repository, specifically the Individual household electric power 
-#consumption Data Set. Then, the program creates a histogram
+#consumption Data Set. Then, the program creates a line graph
 #based upon the Global Active Power field for the days
 #February 1, 2007 through February 2, 2007.
 
@@ -21,9 +21,15 @@ dt <- dt[(dt$Date == "1/2/2007" | dt$Date == "2/2/2007")
 #Convert Global_active_power Column To Numeric to Use With Plot
 dt$Global_active_power <- as.numeric(dt$Global_active_power)
 
-#Create Histogram and Save it as a PNG file
-png(file="plot1.png")
-with(dt, hist(Global_active_power,
-              xlab = "Global Active Power (kilowatts)",
-              main = "Global Active Power", col="red"))
+#Get Date and Time
+date_coll <- strptime(dt$Date,"%d/%m/%Y") #Corrected Format
+date_coll <- paste(date_coll,dt$Time,sep=" ") #Add Time
+date_coll <- as.POSIXlt(date_coll) #Convert To Date Time Class
+
+#Create Line Graph and Save it as a PNG file
+png(file="plot2.png")
+plot(date_coll, dt$Global_active_power,
+     type="l",
+     ylab = "Global Active Power (kilowatts)",
+     xlab = "")
 dev.off()
